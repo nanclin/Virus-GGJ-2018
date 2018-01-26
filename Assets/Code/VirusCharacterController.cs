59bd239bb06cf2e68abd09d7586b63a5dfc4c5c0
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-01-26 23:54:39 +0100

    move towards target

--- Assets/Code/VirusCharacterController.cs
+++ Assets/Code/VirusCharacterController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class VirusCharacterController : MonoBehaviour {
 
     public Transform target;
-    public float speed;
+    public float RotateSpeed;
+    public float WalkSpeed;
 
     // Use this for initialization
     void Start() {
@@ -15,10 +16,13 @@ public class VirusCharacterController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Vector3 targetDir = target.position - transform.position;
-        float step = speed * Time.deltaTime;
+        float rotateStep = RotateSpeed * Time.deltaTime;
         targetDir.y = 0;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateStep, 0.0F);
         Debug.DrawRay(transform.position, newDir, Color.red);
         transform.rotation = Quaternion.LookRotation(newDir);
+
+        float walkStep = WalkSpeed * Time.deltaTime;
+        transform.position += transform.forward * walkStep;
     }
 }
