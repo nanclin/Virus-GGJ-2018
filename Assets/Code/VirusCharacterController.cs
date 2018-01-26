0153ac2ee117760e6240f7e196359285a52e77ab
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-01-27 00:05:10 +0100

    stop following target when reached

--- Assets/Code/VirusCharacterController.cs
+++ Assets/Code/VirusCharacterController.cs
@@ -8,13 +8,21 @@ public class VirusCharacterController : MonoBehaviour {
     public float RotateSpeed;
     public float WalkSpeed;
 
+    private bool FollowTarget;
+
     // Use this for initialization
     void Start() {
-		
+        FollowTarget = true;
     }
 	
     // Update is called once per frame
     void Update() {
+        if (FollowTarget) {
+            FollowingTarget();
+        }
+    }
+
+    private void FollowingTarget() {
         Vector3 targetDir = target.position - transform.position;
         float rotateStep = RotateSpeed * Time.deltaTime;
         targetDir.y = 0;
@@ -24,5 +32,15 @@ public class VirusCharacterController : MonoBehaviour {
 
         float walkStep = WalkSpeed * Time.deltaTime;
         transform.position += transform.forward * walkStep;
+
+        float distanceSquared = Vector3.SqrMagnitude(target.position - transform.position);
+        Debug.Log(distanceSquared);
+        if (distanceSquared < 2f) {
+            OnTargetReached();
+        }
+    }
+
+    private void OnTargetReached() {
+        FollowTarget = false;
     }
 }
