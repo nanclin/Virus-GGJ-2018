d70ee24d67183f1301dcf165329685cd9bc38d29
Luka M <luka.maske@gmail.com>
2018-01-27 02:11:40 +0100

    Added UI

---
+++ Assets/Code/UiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+	private static UiController instance;
+
+	public static UiController Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	public Text scoreText;
+	public Text skillPointsText;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+
+	public void SetScore(int score)
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void SetSkillPoints(int skillPoints)
+	{
+		skillPointsText.text = "Skill points: " + skillPoints;
+	}
+}
