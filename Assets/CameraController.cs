ea4010948a31b446323b95956a03b474b2cb5424
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-01-28 13:20:34 +0100

    camera follow

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float MoveSpeed = 5;
+
+    // Use this for initialization
+    void Start() {
+		
+    }
+	
+    // Update is called once per frame
+    void Update() {
+        Vector3 target = CursorController.instance.transform.position;
+        float dist = (target - transform.position).magnitude;
+        float speed = dist * MoveSpeed;
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+    }
+}
